5dc3f09b8727044e91bb66b0a97633d074963e16
ElisaBorba <elisaborbaa@gmail.com>
2023-11-15 22:28:07 -0300

    req 01

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -2,13 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const fs = require('fs').promises;
-// const path = require('path');
+const path = require('path');
 
-// const talkersPath = path.resolve(__dirname, '../talker.json');
-
-const { join } = require('path');
-
-const talkersPath = join(__dirname, '..', 'talker.json');
+const talkersPath = path.resolve(__dirname, '../talker.json');
 
 const readFile = async () => {
   try {
