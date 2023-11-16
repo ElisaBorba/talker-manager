820111d5153f722805a8cb199570b321fc23b8b5
ElisaBorba <elisaborbaa@gmail.com>
2023-11-15 22:22:45 -0300

    req 01

---
+++ src/routes/apiRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const router = express.Router();
+const fs = require('fs').promises;
+// const path = require('path');
+
+// const talkersPath = path.resolve(__dirname, '../talker.json');
+
+const { join } = require('path');
+
+const talkersPath = join(__dirname, '..', 'talker.json');
+
+const readFile = async () => {
+  try {
+    const data = (await fs.readFile(talkersPath, 'utf-8')) || [];
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+};
+
+const gelAll = async () => {
+  const talkers = await readFile();
+  return talkers;
+};
+
+router.get('/', async (req, res) => {
+  const talkers = await gelAll();
+  res.status(200).json(talkers);
+});
+
+module.exports = router;
